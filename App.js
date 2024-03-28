e8d846e5a79ab79f9115db93d79d6246909f077d
Kevin Kim <kimanikevin254@gmail.com>
2024-03-28 12:56:25 +0300

    set up nested navigators

--- App.js
+++ App.js
@@ -5,17 +5,77 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Player from './screens/Player';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Entypo } from '@expo/vector-icons';
+import Playlists from './screens/Playlists';
 
-const Stack = createNativeStackNavigator();
+// const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const HomeStack = createNativeStackNavigator();
+const PlaylistsStack = createNativeStackNavigator();
+
+function HomeStackScreen(){
+  return (
+    <HomeStack.Navigator screenOptions={{ headerShown: false }}>
+      <HomeStack.Screen 
+        name='Home'
+        component={Home}
+      />
+
+      <HomeStack.Screen 
+        name='Player'
+        component={Player}
+      />
+    </HomeStack.Navigator>
+  )
+}
+
+function PlaylistsStackScreen() {
+  return (
+    <PlaylistsStack.Navigator>
+      <PlaylistsStack.Screen
+        name="Playlists"
+        component={Playlists}
+      />
+    </PlaylistsStack.Navigator>
+  );
+}
 
 export default function App() {
     return (
     <Provider store={store}>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName='Home' screenOptions={{ headerShown: false }}>
-          <Stack.Screen name='Home' component={Home} />
-          <Stack.Screen name='Player' component={Player} />
-        </Stack.Navigator>
+        <Tab.Navigator 
+          screenOptions={({ route }) => ({
+            headerShown: false,
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+
+              if(route.name === 'Home'){
+                iconName = focused ?
+                'home' :
+                'home'
+              } else if(route.name === 'Playlists'){
+                iconName = focused ?
+                'list' :
+                'list'
+              }
+
+              return <Entypo name={iconName} size={24} color="black" />
+            }
+          })}
+        >
+          <Tab.Screen 
+            name='Home'
+            component={HomeStackScreen}
+          />
+
+          <Tab.Screen 
+            name='Playlists'
+            component={PlaylistsStackScreen}
+          />
+        </Tab.Navigator>
       </NavigationContainer>
     </Provider>
   );
---
+++ screens/Playlists.js
@@ -0,0 +1,11 @@
+import { SafeAreaView, Text, View } from "react-native";
+
+export default function Playlists(){
+    return (
+        <SafeAreaView className='flex-1'>
+            <View className='flex-1 items  justify-center'>
+                <Text>Playlists</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
